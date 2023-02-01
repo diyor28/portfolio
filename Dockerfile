48ab2a905a8b15e7a09fac5e4f61cb252daa0a97
Diyor Khaydarov <diyor28july@gmail.com>
2023-02-01 08:15:45 +0500

    add rm bot

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,3 @@ FROM certbot/certbot:v1.27.0 as cert-bot
 COPY ssl.sh /entrypoint.sh
 RUN chmod 755 /entrypoint.sh
 ENTRYPOINT ["/entrypoint.sh"]
-CMD ["diyor28.com", "traefik.diyor28.com", "notagar.diyor28.com"]
--- ssl.sh
+++ ssl.sh
@@ -1,18 +1,24 @@
 #!/bin/sh
 if ! test -f /etc/letsencrypt/live/diyor28.com/privkey.pem; then
-    certbot certonly --agree-tos --non-interactive --renew-by-default --email diyor28july@gmail.com --standalone --preferred-challenges http -d "$1"
+    certbot certonly --agree-tos --non-interactive --renew-by-default --email diyor28july@gmail.com --standalone --preferred-challenges http -d diyor28.com
 else
-    certbot certonly --standalone --force-renew -d "$1"
+    certbot certonly --standalone --force-renew -d diyor28.com
 fi
 
 if ! test -f /etc/letsencrypt/live/traefik.diyor28.com/privkey.pem; then
-    certbot certonly --agree-tos --non-interactive --renew-by-default --email diyor28july@gmail.com --standalone --preferred-challenges http -d "$2"
+    certbot certonly --agree-tos --non-interactive --renew-by-default --email diyor28july@gmail.com --standalone --preferred-challenges http -d traefik.diyor28.com
 else
-    certbot certonly --standalone --force-renew -d "$2"
+    certbot certonly --standalone --force-renew -d traefik.diyor28.com
 fi
 
 if ! test -f /etc/letsencrypt/live/notagar.diyor28.com/privkey.pem; then
-    certbot certonly --agree-tos --non-interactive --renew-by-default --email diyor28july@gmail.com --standalone --preferred-challenges http -d "$3"
+    certbot certonly --agree-tos --non-interactive --renew-by-default --email diyor28july@gmail.com --standalone --preferred-challenges http -d notagar.diyor28.com
 else
-    certbot certonly --standalone  --force-renew -d "$3"
+    certbot certonly --standalone --force-renew -d notagar.diyor28.com
+fi
+
+if ! test -f /etc/letsencrypt/live/rm.diyor28.com/privkey.pem; then
+    certbot certonly --agree-tos --non-interactive --renew-by-default --email diyor28july@gmail.com --standalone --preferred-challenges http -d rm.diyor28.com
+else
+    certbot certonly --standalone --force-renew -d rm.diyor28.com
 fi
